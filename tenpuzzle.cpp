83e05768ccbc18d9cd6e9a5b9693f8f59aba30e3
Hironori Komatsu <mr.first.base6011@gmail.com>
2026-02-26 11:52:02 +0900

    update tenpuzzle.cpp

--- tenpuzzle.cpp
+++ tenpuzzle.cpp
@@ -2,156 +2,155 @@
 #include <string>
 #include <vector>
 #include <stack>
-using namespace std;
+#include <cmath>
 
-
-//RPN計算
-double solve(const vector<char>& expr){
-    stack<double> st;
-
-    for(char c : expr){
-      switch(c){
-      
-        case '+': {
-          double right = st.top(); st.pop();
-          double left = st.top(); st.pop();
-          st.push(left + right);
-          break;
-        }
-
-        case '-' :{
-          double right = st.top(); st.pop();
-          double left = st.top(); st.pop();
-          st.push(left - right);
-          break;
-        }
-
-        case '*' :{
-          double right = st.top(); st.pop();
-          double left = st.top(); st.pop();
-          st.push(left * right);
-          break;
-        }
-
-        case '/' :{
-          double right = st.top(); st.pop();
-          double left = st.top(); st.pop();
-          st.push(left / right);
-          break;
-        }
-
-        default:{
-          st.push(c - '0');
-          break;
-        }
-      }
-    }
-    
-  return st.top();
-}
-
-//演算子の優先順位
-int priority(char op){
-  if(op == '+' || op == '-') return 2;
-  if(op == '*' || op == '/') return 3;
+//計算
+double calc(double left, double right, char op){
+  if(op == '+'){
+    return left + right;
+  }
+  if(op == '-'){
+    return left -right;
+  }
+  if(op == '*'){
+    return left * right;
+  }
+  if(op == '/'){
+    return left / right;
+  }
 
   return 0;
 }
 
-//中置換→後置換
-vector<char> replace(const vector<char>& expr){
-  vector<char> rpn_queue;
-  stack<char> op_stack;
+//演算子の優先順位
+int getPriority(char op){
+  if(op == '+' || op == '-'){
+    return 2;
+  }  
+  if(op == '*' || op == '/'){
+    return 3;
+  }  
 
-  for(char c : expr){
-
-    //数字の場合
-    if(isdigit(c)){
-      rpn_queue.push_back(c);
-    }
-    //演算子の場合
-    else if(priority(c) > 0){
-      //空ではなく且つop_stackの演算子の優先度が入力する演算子より高い場合に(*,/の場合) 
-      while(!op_stack.empty() && priority(op_stack.top()) >= priority(c)){
-        rpn_queue.push_back(op_stack.top());
-        op_stack.pop();
-      }
-      op_stack.push(c);
-    }
-  }
-
-  //残りの演算子をrpn_queueに
-  while(!op_stack.empty()){
-  rpn_queue.push_back(op_stack.top()); 
-  op_stack.pop();
-  }
-  
-  return rpn_queue;
+  return 0;
 }
+
+//式の評価
+double evaluateExpression(const std::vector<char>& expr){
+  std::stack<double> num_stack;
+  std::stack<char> op_stack;
+
+    for(char c : expr){
+        //数字の場合
+        if(isdigit(c)){
+          num_stack.push(c - '0');
+        }//演算子の場合
+        else if(getPriority(c) > 0){
+          //空ではなく且つop_stackの演算子の優先度が入力する演算子より高い場合に(*,/の場合) 
+          while(!op_stack.empty() && getPriority(op_stack.top()) >= getPriority(c)){
+              double right = num_stack.top();
+              num_stack.pop();
+
+              double left = num_stack.top();
+              num_stack.pop();
+
+              char op = op_stack.top();
+              op_stack.pop();
+
+              num_stack.push(calc(left,right,op));
+          }
+
+          op_stack.push(c);
+        }
+    }//残りの演算子
+    while(!op_stack.empty()){
+        double right = num_stack.top();
+        num_stack.pop();
+
+        double left = num_stack.top();
+        num_stack.pop();
+
+        char op = op_stack.top();
+        op_stack.pop();
+
+        num_stack.push(calc(left,right,op));
+    }
+
+  return num_stack.top();
+}
+
+//修正中(全角の場合について)
+//入力評価
+bool evaluateInput(std::string input,std::vector<char>& num){
+    
+   if(input.size() < 4){
+      std::cout << "4つより少なく入力されています\n4つの数字を入力してください" << std::endl;
+      return false;
+    }
+
+    if(input.size() > 4){
+      std::cout << "4つより多く入力されています\n4つの数字を入力してください" << std::endl;
+      return false;
+    }
+
+    for(char c : input){
+      if(!isdigit(c)){
+        std::cout << "文字が入力されています\n4つの数字を入力してください" << std::endl; 
+        return false;
+      }
+    }
+
+    for(char c : input){
+      num.push_back(c);
+    }
+
+    return true;
+}
+ 
+
 
 
 int main(){
 
-  string input;
-  vector<char> num;
+  std::string input;
+  std::vector<char> num;
   bool validated = false;
 
- //修正中
  //入力
+  std::cout << "4つの数字を入力してください" << std::endl;
   while(!validated){
-    cout << "4つの数字を入力してください" << endl;
-    cin >> input;
 
-    if(input.size() == 4){
-        for(int i = 0; i < input.size(); i++){
-          if(isdigit(input[i])){
-            num.push_back(input[i]);
-            validated = true;
-          }
-          else{
-            cout << "文字が入力されています" << endl;
-            break;
-          }
-        }
-    }
-    else if(input.size() < 4){
-      cout << "4つより少なく入力されています" << endl;
-    }
-    else{
-      cout << "4つより多く入力されています" << endl;
-    }
+    std::cin >> input;
+    validated = evaluateInput(input,num);
+
   }
-
-  
-  
-  char a = num[0];
-  char b = num[1];
-  char c = num[2];
-  char d = num[3];
-
 
   //64通り
-  vector<char> ops = {'+','-','*','/'};
+  std::vector<char> arith_ops = {'+','-','*','/'};
 
-  for(char op1 : ops){
-    for(char op2 : ops){
-       for(char op3 : ops){
-          vector<char> expr = {a,op1,b,op2,c,op3,d};
+      for(char op1 : arith_ops){
+        for(char op2 : arith_ops){
+          for(char op3 : arith_ops){
+              
+              char ops[] = {op1,op2,op3};
 
-          vector<char> rpn = replace(expr);
+              for(int j = 0; j < sizeof(ops); j++){
+                char op = ops[j];
+              }
 
-          double answer = solve(rpn);
+              std::vector<char> expr = {num[0],op1,num[1],op2,num[2],op3,num[3]};
+              double answer = evaluateExpression(expr);
+              double roundAnswer = round(answer * 100) / 100;
 
-          if(answer == 10){
-            for(char rpn_expr : rpn) 
-            cout << rpn_expr;
-            cout << "\n";
-            cout << answer << endl;
+              if(roundAnswer == 10){
+                for(char c : expr){
+                  std::cout << c;
+                }
+                std::cout << "\n"; 
+                std::cout << roundAnswer << std::endl;
+              }
           }
-
-       }
-    }
-  }
+        }
+      }
 
   return 0;
 }
